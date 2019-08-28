3e0851f4a0ad584bfb257de94448076c49fa08e7
MinyoungKoo <rnalsdud1014@naver.com>
2019-08-28 23:17:54 +0900

    회원가입(기본정보) 화면 구성 - imageView, TextField, TextView, Button

---
+++ 02SignUp/02SignUp/BasicInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BasicInfoViewController.swift
+//  02SignUp
+//
+//  Created by 구민영 on 28/08/2019.
+//  Copyright © 2019 구민영. All rights reserved.
+//
+
+import UIKit
+
+class BasicInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
