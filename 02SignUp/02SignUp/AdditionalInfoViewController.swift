88fe26882c8825613d0348bec1cfd9dd1553efac
MinyoungKoo <rnalsdud1014@naver.com>
2020-03-08 17:33:54 +0900

    기능 : 생년월일 선택 및 화면 전환 기능 추가
    - UIDatePicker로 선택한 날짜가 생년월일 레이블에 즉시 반영된다.
    - '이전' 버튼을 클릭하면 기본정보 입력 화면으로 돌아간다.
    - '취소' 버튼을 클릭하면 로그인 화면으로 돌아간다.

--- 02SignUp/02SignUp/AdditionalInfoViewController.swift
+++ 02SignUp/02SignUp/AdditionalInfoViewController.swift
@@ -9,22 +9,55 @@
 import UIKit
 
 class AdditionalInfoViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK:- IBOutlets
+    @IBOutlet weak var cancelButton: UIButton?
+    @IBOutlet weak var backButton: UIButton?
+    @IBOutlet weak var signUpButton: UIButton?
+    @IBOutlet weak var birthLabel: UILabel?
+    @IBOutlet weak var datePicker: UIDatePicker?
+    
+    // MARK:- Methods
+    // MARK: Life Cycle
+    override func viewWillAppear(_ animated: Bool) {
+        guard let datePicker = datePicker else {return}
+        self.pickerValueChanged(datePicker)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.addTargets()
     }
-    */
+    
+    // MARK: Custom Method
+    func addTargets() {
+        cancelButton?.addTarget(self, action: #selector(touchUpCancelButton), for: .touchUpInside)
+        backButton?.addTarget(self, action: #selector(touchUpBackButton), for: .touchUpInside)
+        signUpButton?.addTarget(self, action: #selector(touchUpSignUpButton), for: .touchUpInside)
+        datePicker?.addTarget(self, action: #selector(pickerValueChanged), for: .valueChanged)
+    }
+    
+    // MARK: Action Method
+    @objc func pickerValueChanged(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        guard let displayedDate = datePicker?.date else {return}
+        birthLabel?.text = dateFormatter.string(from: displayedDate)
+    }
+    
+    @objc func touchUpCancelButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        // 데이터 초기화
+    }
 
+    @objc func touchUpBackButton(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+        // 데이터 유지
+    }
+    
+    @objc func touchUpSignUpButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        // 다음 뷰에서 텍스트필드에 ID 표시
+    }
 }
+
+
